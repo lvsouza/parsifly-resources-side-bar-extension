1c7cf118077c38caf98f32bcc130e3cba60f4428
Lucas Souza <ls18017@gmail.com>
2025-11-29 13:25:50 -0300

    feat: add context menu for items

--- src/extension.ts
+++ src/extension.ts
@@ -1,28 +1,83 @@
-import { ExtensionBase, TabsView, ListProvider, Action, TabView, ListViewItem, IPage, IComponent, IService, IProject, TProjectType, IFolder } from 'parsifly-extension-base';
+import { ExtensionBase, TabsView, ListProvider, Action, TabView, ListViewItem, IPage, IComponent, IService, IProject, TProjectType, IFolder, ContextMenuItem, ICollection } from 'parsifly-extension-base';
 
 // Envs.DEBUG = true;
 
 
 new class Extension extends ExtensionBase {
 
-  loadPages(content: (IPage | IFolder<IPage>)[]): ListViewItem[] {
-    return content.map(item => {
+  async loadPages(ref: ICollection<IPage | IFolder<IPage>>): Promise<ListViewItem[]> {
+    const items = await ref.value();
+
+    return items.map(item => {
       if (item.type === 'folder') {
         return new ListViewItem({
           key: item.id,
           children: true,
           label: item.name,
           icon: 'VscFolder',
+          getContextMenuItems: async () => {
+            return [
+              new ContextMenuItem({
+                label: 'Delete',
+                icon: 'VscTrash',
+                key: `delete:${item.id}`,
+                description: 'This action is irreversible',
+                onClick: async () => {
+                  await ref.doc(item.id).delete()
+                },
+              }),
+              new ContextMenuItem({
+                label: 'New page',
+                icon: 'VscNewFile',
+                key: `new-page:${item.id}`,
+                description: 'Add to this folder a new page',
+                onClick: async () => {
+                  const name = await this.application.commands.editor.showQuickPick({
+                    title: 'Page name',
+                    placeholder: 'Example: Page1',
+                    helpText: 'Type the name of the page.',
+                  });
+                  if (!name) return;
+
+                  await ref.doc(item.id).collection('content').add({
+                    name: name,
+                    type: 'page',
+                    description: '',
+                    id: crypto.randomUUID(),
+                  });
+                },
+              }),
+              new ContextMenuItem({
+                label: 'New folder',
+                icon: 'VscNewFolder',
+                key: `new-folder:${item.id}`,
+                description: 'Add to this folder a new folder',
+                onClick: async () => {
+                  const name = await this.application.commands.editor.showQuickPick({
+                    title: 'Folder name',
+                    placeholder: 'Example: Folder1',
+                    helpText: 'Type the name of the folder.',
+                  });
+                  if (!name) return;
+
+                  await ref.doc(item.id).collection('content').add({
+                    name: name,
+                    content: [],
+                    type: 'folder',
+                    description: '',
+                    id: crypto.randomUUID(),
+                  });
+                },
+              }),
+            ];
+          },
           getItems: async () => {
-            const items = this.loadPages(item.content)
+            const items = await this.loadPages(ref.doc(item.id).collection('content'))
             return items
           },
           onItemClick: async () => {
             await this.application.selection.select(item.id);
           },
-          onItemDoubleClick: async () => {
-            await this.application.edition.open(item.id);
-          }
         })
       }
 
@@ -36,29 +91,97 @@ new class Extension extends ExtensionBase {
         },
         onItemDoubleClick: async () => {
           await this.application.edition.open(item.id);
-        }
+        },
+        getContextMenuItems: async () => {
+          return [
+            new ContextMenuItem({
+              label: 'Delete',
+              icon: 'VscTrash',
+              key: `delete:${item.id}`,
+              description: 'This action is irreversible',
+              onClick: async () => {
+                await ref.doc(item.id).delete()
+              },
+            }),
+          ];
+        },
       });
     })
   }
 
-  loadComponents(content: (IComponent | IFolder<IComponent>)[]): ListViewItem[] {
-    return content.map(item => {
+  async loadComponents(ref: ICollection<IComponent | IFolder<IComponent>>): Promise<ListViewItem[]> {
+    const items = await ref.value();
+
+    return items.map(item => {
       if (item.type === 'folder') {
         return new ListViewItem({
           key: item.id,
           children: true,
           label: item.name,
           icon: 'VscFolder',
+          getContextMenuItems: async () => {
+            return [
+              new ContextMenuItem({
+                label: 'Delete',
+                icon: 'VscTrash',
+                key: `delete:${item.id}`,
+                description: 'This action is irreversible',
+                onClick: async () => {
+                  await ref.doc(item.id).delete()
+                },
+              }),
+              new ContextMenuItem({
+                label: 'New component',
+                icon: 'VscNewFile',
+                key: `new-component:${item.id}`,
+                description: 'Add to this folder a new component',
+                onClick: async () => {
+                  const name = await this.application.commands.editor.showQuickPick({
+                    title: 'Component name?',
+                    placeholder: 'Example: Component1',
+                    helpText: 'Type the name of the component.',
+                  });
+                  if (!name) return;
+
+                  await ref.doc(item.id).collection('content').add({
+                    name: name,
+                    description: '',
+                    type: 'component',
+                    id: crypto.randomUUID(),
+                  });
+                },
+              }),
+              new ContextMenuItem({
+                label: 'New folder',
+                icon: 'VscNewFolder',
+                key: `new-folder:${item.id}`,
+                description: 'Add to this folder a new folder',
+                onClick: async () => {
+                  const name = await this.application.commands.editor.showQuickPick({
+                    title: 'Folder name',
+                    placeholder: 'Example: Folder1',
+                    helpText: 'Type the name of the folder.',
+                  });
+                  if (!name) return;
+
+                  await ref.doc(item.id).collection('content').add({
+                    name: name,
+                    content: [],
+                    type: 'folder',
+                    description: '',
+                    id: crypto.randomUUID(),
+                  });
+                },
+              }),
+            ];
+          },
           getItems: async () => {
-            const items = this.loadComponents(item.content)
+            const items = await this.loadComponents(ref.doc(item.id).collection('content'))
             return items
           },
           onItemClick: async () => {
             await this.application.selection.select(item.id);
           },
-          onItemDoubleClick: async () => {
-            await this.application.edition.open(item.id);
-          }
         })
       }
 
@@ -78,29 +201,97 @@ new class Extension extends ExtensionBase {
         },
         onItemDoubleClick: async () => {
           await this.application.edition.open(item.id);
-        }
+        },
+        getContextMenuItems: async () => {
+          return [
+            new ContextMenuItem({
+              label: 'Delete',
+              icon: 'VscTrash',
+              key: `delete:${item.id}`,
+              description: 'This action is irreversible',
+              onClick: async () => {
+                await ref.doc(item.id).delete()
+              },
+            }),
+          ];
+        },
       });
     })
   }
 
-  loadServices(content: (IService | IFolder<IService>)[]): ListViewItem[] {
-    return content.map(item => {
+  async loadServices(ref: ICollection<IService | IFolder<IService>>): Promise<ListViewItem[]> {
+    const items = await ref.value();
+
+    return items.map(item => {
       if (item.type === 'folder') {
         return new ListViewItem({
           key: item.id,
           children: true,
           label: item.name,
           icon: 'VscFolder',
+          getContextMenuItems: async () => {
+            return [
+              new ContextMenuItem({
+                label: 'Delete',
+                icon: 'VscTrash',
+                key: `delete:${item.id}`,
+                description: 'This action is irreversible',
+                onClick: async () => {
+                  await ref.doc(item.id).delete()
+                },
+              }),
+              new ContextMenuItem({
+                label: 'New service',
+                icon: 'VscNewFile',
+                key: `new-service:${item.id}`,
+                description: 'Add to this folder a new service',
+                onClick: async () => {
+                  const name = await this.application.commands.editor.showQuickPick({
+                    title: 'Service name?',
+                    placeholder: 'Example: Service1',
+                    helpText: 'Type the name of the service.',
+                  });
+                  if (!name) return;
+
+                  await ref.doc(item.id).collection('content').add({
+                    name: name,
+                    description: '',
+                    type: 'service',
+                    id: crypto.randomUUID(),
+                  });
+                },
+              }),
+              new ContextMenuItem({
+                label: 'New folder',
+                icon: 'VscNewFolder',
+                key: `new-folder:${item.id}`,
+                description: 'Add to this folder a new folder',
+                onClick: async () => {
+                  const name = await this.application.commands.editor.showQuickPick({
+                    title: 'Folder name',
+                    placeholder: 'Example: Folder1',
+                    helpText: 'Type the name of the folder.',
+                  });
+                  if (!name) return;
+
+                  await ref.doc(item.id).collection('content').add({
+                    name: name,
+                    content: [],
+                    type: 'folder',
+                    description: '',
+                    id: crypto.randomUUID(),
+                  });
+                },
+              }),
+            ];
+          },
           getItems: async () => {
-            const items = this.loadServices(item.content)
+            const items = await this.loadServices(ref.doc(item.id).collection('content'))
             return items
           },
           onItemClick: async () => {
             await this.application.selection.select(item.id);
           },
-          onItemDoubleClick: async () => {
-            await this.application.edition.open(item.id);
-          }
         })
       }
 
@@ -114,7 +305,20 @@ new class Extension extends ExtensionBase {
         },
         onItemDoubleClick: async () => {
           await this.application.edition.open(item.id);
-        }
+        },
+        getContextMenuItems: async () => {
+          return [
+            new ContextMenuItem({
+              label: 'Delete',
+              icon: 'VscTrash',
+              key: `delete:${item.id}`,
+              description: 'This action is irreversible',
+              onClick: async () => {
+                await ref.doc(item.id).delete()
+              },
+            }),
+          ];
+        },
       });
     })
   }
@@ -207,8 +411,8 @@ new class Extension extends ExtensionBase {
         dataProvider: new ListProvider({
           key: 'data-provider',
           getItems: async () => {
-            const pages = await this.application.dataProviders.project().collection<IProject<TProjectType>['pages'][number]>('pages').value();
-            const items = this.loadPages(pages)
+            const pagesRef = this.application.dataProviders.project().collection<IProject<TProjectType>['pages'][number]>('pages');
+            const items = await this.loadPages(pagesRef)
             return items
           },
         }),
@@ -218,8 +422,8 @@ new class Extension extends ExtensionBase {
         dataProvider: new ListProvider({
           key: 'data-provider',
           getItems: async () => {
-            const components = await this.application.dataProviders.project().collection<IComponent>('components').value();
-            const items = this.loadComponents(components)
+            const componentsRef = this.application.dataProviders.project().collection<IProject<TProjectType>['components'][number]>('components');
+            const items = await this.loadComponents(componentsRef)
             return items
           },
         }),
@@ -229,8 +433,8 @@ new class Extension extends ExtensionBase {
         dataProvider: new ListProvider({
           key: 'data-provider',
           getItems: async () => {
-            const services = await this.application.dataProviders.project().collection<IService>('services').value();
-            const items = this.loadServices(services)
+            const servicesRef = this.application.dataProviders.project().collection<IProject<TProjectType>['services'][number]>('services');
+            const items = await this.loadServices(servicesRef)
             return items
           },
         }),
