4803fa442edc611f1a9f40af22ab9aeb251d61b5
Lucas Souza <ls18017@gmail.com>
2025-11-29 17:56:13 -0300

    refactor: improve resources side bar with new base versions

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,4 @@
-import { ExtensionBase, TabsView, ListProvider, Action, TabView, ListViewItem, IPage, IComponent, IService, IProject, TProjectType, IFolder, ContextMenuItem, ICollection } from 'parsifly-extension-base';
+import { ExtensionBase, TabsView, ListProvider, TabView, ListViewItem, IPage, IComponent, IService, IProject, TProjectType, IFolder, ContextMenuItem, ICollection } from 'parsifly-extension-base';
 
 // Envs.DEBUG = true;
 
@@ -325,95 +325,79 @@ new class Extension extends ExtensionBase {
 
   resourcesListView = new TabsView({
     key: 'resources-side-bar',
-    actions: [
-      new Action({
-        key: 'add-resources',
-        action: async () => {
-          const type = await this.application.commands.editor.showQuickPick({
-            title: 'Type of resource?',
-            placeholder: 'Example: page',
-            helpText: '"page", "component" or "service"',
-          });
-
-          if (!type) return;
-          if (!['page', 'component', 'service'].includes(type)) {
-            this.application.commands.editor.feedback(`Type of resource ${type} not valid. Please use "page", "component" or "service".`, 'error')
-            return;
-          }
-
-          const name = await this.application.commands.editor.showQuickPick({
-            title: 'Name of resource?',
-            placeholder: 'Example: Resource1',
-            helpText: 'Type the name of the resource.',
-          });
-          if (!name) return;
-
-          if (type === 'page') {
-            this.application.dataProviders.project().collection<IPage>('pages').add({
-              name: name,
-              type: 'page',
-              description: '',
-              id: crypto.randomUUID(),
-            });
-          } else if (type === 'component') {
-            this.application.dataProviders.project().collection<IComponent>('components').add({
-              name: name,
-              description: '',
-              type: 'component',
-              id: crypto.randomUUID(),
-            });
-          } else if (type === 'service') {
-            this.application.dataProviders.project().collection<IService>('services').add({
-              name: name,
-              description: '',
-              type: 'service',
-              id: crypto.randomUUID(),
-            });
-          }
-
-          this.application.views.refresh(this.resourcesListView);
-        }
-      }),
-      new Action({
-        key: 'add-folders',
-        action: async () => {
-          const type = await this.application.commands.editor.showQuickPick({
-            title: 'Where to put the folder?',
-            placeholder: 'Example: pages',
-            helpText: '"pages", "components" or "services"',
-          });
-          if (!type) return;
-          if (!['pages', 'components', 'services'].includes(type)) {
-            this.application.commands.editor.feedback(`Location of resource ${type} not valid. Please use "pages", "components" or "services".`, 'error')
-            return;
-          }
-
-          const name = await this.application.commands.editor.showQuickPick({
-            title: 'Name of folder?',
-            placeholder: 'Example: Folder',
-            helpText: 'Type the name of the folder.',
-          });
-          if (!name) return;
-
-          this.application.dataProviders.project().collection<IFolder>(type).add({
-            name: name,
-            content: [],
-            type: 'folder',
-            description: '',
-            id: crypto.randomUUID(),
-          });
-        }
-      }),
-    ],
     tabs: [
       new TabView({
         key: 'tab-pages',
         dataProvider: new ListProvider({
           key: 'data-provider',
           getItems: async () => {
-            const pagesRef = this.application.dataProviders.project().collection<IProject<TProjectType>['pages'][number]>('pages');
-            const items = await this.loadPages(pagesRef)
-            return items
+            const projectId = await this.application.dataProviders.project().field('id').value();
+            const projectName = await this.application.dataProviders.project().field('name').value();
+            const projectDescription = await this.application.dataProviders.project().field('description').value();
+            return [
+              new ListViewItem({
+                key: projectId,
+                children: true,
+                label: projectName,
+                icon: 'VscRootFolderOpened',
+                description: projectDescription || '',
+                onItemClick: async () => {
+                  await this.application.selection.select(projectId);
+                },
+                getItems: async () => {
+                  const pagesRef = this.application.dataProviders.project().collection<IProject<TProjectType>['pages'][number]>('pages');
+                  const items = await this.loadPages(pagesRef)
+                  return items;
+                },
+                getContextMenuItems: async () => {
+                  return [
+                    new ContextMenuItem({
+                      label: 'New page',
+                      icon: 'VscNewFile',
+                      key: `new-page:${projectId}`,
+                      description: 'Add to this folder a new page',
+                      onClick: async () => {
+                        const name = await this.application.commands.editor.showQuickPick({
+                          title: 'Page name',
+                          placeholder: 'Example: Page1',
+                          helpText: 'Type the name of the page.',
+                        });
+                        if (!name) return;
+
+                        await this.application.dataProviders.project().collection('pages').add({
+                          name: name,
+                          type: 'page',
+                          description: '',
+                          id: crypto.randomUUID(),
+                        });
+                      },
+                    }),
+                    new ContextMenuItem({
+                      label: 'New folder',
+                      icon: 'VscNewFolder',
+                      key: `new-folder:${projectId}`,
+                      description: 'Add to this folder a new folder',
+                      onClick: async () => {
+                        const name = await this.application.commands.editor.showQuickPick({
+                          title: 'Folder name',
+                          placeholder: 'Example: Folder1',
+                          helpText: 'Type the name of the folder.',
+                        });
+                        if (!name) return;
+
+                        await this.application.dataProviders.project().collection('pages').add({
+                          name: name,
+                          content: [],
+                          type: 'folder',
+                          description: '',
+                          id: crypto.randomUUID(),
+                        });
+                      },
+                    }),
+                  ]
+                }
+              })
+            ];
           },
         }),
       }),
@@ -422,9 +406,73 @@ new class Extension extends ExtensionBase {
         dataProvider: new ListProvider({
           key: 'data-provider',
           getItems: async () => {
-            const componentsRef = this.application.dataProviders.project().collection<IProject<TProjectType>['components'][number]>('components');
-            const items = await this.loadComponents(componentsRef)
-            return items
+            const projectId = await this.application.dataProviders.project().field('id').value();
+            const projectName = await this.application.dataProviders.project().field('name').value();
+            const projectDescription = await this.application.dataProviders.project().field('description').value();
+            return [
+              new ListViewItem({
+                key: projectId,
+                children: true,
+                label: projectName,
+                icon: 'VscRootFolderOpened',
+                description: projectDescription || '',
+                onItemClick: async () => {
+                  await this.application.selection.select(projectId);
+                },
+                getItems: async () => {
+                  const componentsRef = this.application.dataProviders.project().collection<IProject<TProjectType>['components'][number]>('components');
+                  const items = await this.loadComponents(componentsRef)
+                  return items;
+                },
+                getContextMenuItems: async () => {
+                  return [
+                    new ContextMenuItem({
+                      label: 'New component',
+                      icon: 'VscNewFile',
+                      key: `new-component:${projectId}`,
+                      description: 'Add to this folder a new component',
+                      onClick: async () => {
+                        const name = await this.application.commands.editor.showQuickPick({
+                          title: 'Component name?',
+                          placeholder: 'Example: Component1',
+                          helpText: 'Type the name of the component.',
+                        });
+                        if (!name) return;
+
+                        await this.application.dataProviders.project().collection('components').add({
+                          name: name,
+                          description: '',
+                          type: 'component',
+                          id: crypto.randomUUID(),
+                        });
+                      },
+                    }),
+                    new ContextMenuItem({
+                      label: 'New folder',
+                      icon: 'VscNewFolder',
+                      key: `new-folder:${projectId}`,
+                      description: 'Add to this folder a new folder',
+                      onClick: async () => {
+                        const name = await this.application.commands.editor.showQuickPick({
+                          title: 'Folder name',
+                          placeholder: 'Example: Folder1',
+                          helpText: 'Type the name of the folder.',
+                        });
+                        if (!name) return;
+
+                        await this.application.dataProviders.project().collection('components').add({
+                          name: name,
+                          content: [],
+                          type: 'folder',
+                          description: '',
+                          id: crypto.randomUUID(),
+                        });
+                      },
+                    }),
+                  ]
+                }
+              })
+            ];
           },
         }),
       }),
@@ -433,9 +481,73 @@ new class Extension extends ExtensionBase {
         dataProvider: new ListProvider({
           key: 'data-provider',
           getItems: async () => {
-            const servicesRef = this.application.dataProviders.project().collection<IProject<TProjectType>['services'][number]>('services');
-            const items = await this.loadServices(servicesRef)
-            return items
+            const projectId = await this.application.dataProviders.project().field('id').value();
+            const projectName = await this.application.dataProviders.project().field('name').value();
+            const projectDescription = await this.application.dataProviders.project().field('description').value();
+            return [
+              new ListViewItem({
+                key: projectId,
+                children: true,
+                label: projectName,
+                icon: 'VscRootFolderOpened',
+                description: projectDescription || '',
+                onItemClick: async () => {
+                  await this.application.selection.select(projectId);
+                },
+                getItems: async () => {
+                  const servicesRef = this.application.dataProviders.project().collection<IProject<TProjectType>['services'][number]>('services');
+                  const items = await this.loadServices(servicesRef)
+                  return items;
+                },
+                getContextMenuItems: async () => {
+                  return [
+                    new ContextMenuItem({
+                      label: 'New service',
+                      icon: 'VscNewFile',
+                      key: `new-service:${projectId}`,
+                      description: 'Add to this folder a new service',
+                      onClick: async () => {
+                        const name = await this.application.commands.editor.showQuickPick({
+                          title: 'Service name?',
+                          placeholder: 'Example: Service1',
+                          helpText: 'Type the name of the service.',
+                        });
+                        if (!name) return;
+
+                        await this.application.dataProviders.project().collection('services').add({
+                          name: name,
+                          description: '',
+                          type: 'service',
+                          id: crypto.randomUUID(),
+                        });
+                      },
+                    }),
+                    new ContextMenuItem({
+                      label: 'New folder',
+                      icon: 'VscNewFolder',
+                      key: `new-folder:${projectId}`,
+                      description: 'Add to this folder a new folder',
+                      onClick: async () => {
+                        const name = await this.application.commands.editor.showQuickPick({
+                          title: 'Folder name',
+                          placeholder: 'Example: Folder1',
+                          helpText: 'Type the name of the folder.',
+                        });
+                        if (!name) return;
+
+                        await this.application.dataProviders.project().collection('services').add({
+                          name: name,
+                          content: [],
+                          type: 'folder',
+                          description: '',
+                          id: crypto.randomUUID(),
+                        });
+                      },
+                    }),
+                  ]
+                }
+              })
+            ];
           },
         }),
       }),
